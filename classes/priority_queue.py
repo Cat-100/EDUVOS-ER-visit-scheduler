a6d1a5cdcf5f372c3fd3cada516b651a8fd61ee1
Cat-100 <saskianlouw.werk7@gmail.com>
2024-10-13 22:56:32 +0200

    initial commit

---
+++ classes/priority_queue.py
@@ -0,0 +1,13 @@
+class PriorityQueue:
+    ''' 
+    
+    Priority Queue that utilizes the Binary Heap tree structure to queue, dequeue, 
+    and ensure quick retrieval of `patients` based on their `priority level` 
+    
+    **Why Binary Heap tree structure?**
+    
+    Lists commonly ustilize contingious memory blocks, meaning that arrays need to be appended 
+    and unneccessary memory blocks are used to grow an list/array dynamically.
+
+    Since the 
+    '''
