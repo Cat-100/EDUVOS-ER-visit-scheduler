4b75a1bc5beef91fd05704c52a5d7acebee47b94
Cat-100 <saskianlouw.werk7@gmail.com>
2024-10-14 01:20:10 +0200

    Added the entire priority queue for the application whilst having changed the patient class hinting and added a few helper functions to add in the queue. Added addtional commenting on the priority queue class.

--- classes/priority_queue.py
+++ classes/priority_queue.py
@@ -1,6 +1,8 @@
+from classes.patient import Patient
+from typing import List
+from utils.helpers.helper_functions import SHelperFunctions
 class PriorityQueue:
     ''' 
-    
     Priority Queue that utilizes the Binary Heap tree structure to queue, dequeue, 
     and ensure quick retrieval of `patients` based on their `priority level` 
     
@@ -36,7 +38,106 @@ class PriorityQueue:
     # ------------------------------ Functions ------------------------------- #
     def __init__(self) -> None:
         ''' Class Constructor '''
-        # Create nodes to hold all the elements
-        self.nodes = []
+        # Create list of nodes to hold all the elements
+        self.nodes : List[Patient] = []
+
+    def enqueue(self, patient : Patient) -> None:
+        '''
+        Ádds a Patient to the queue. 
+        
+        Will heapify based on the patient's priority level. 5 being the most sever.
+        '''
+        # Add patient to the end of the list
+        self.nodes.append(patient)
+
+        # Heapify the nodes
+        self._heapify(len(self.nodes) - 1)
+
+    def _heapify(self, index: int) -> None:
+        '''Moves current node on the tree based on the priority level'''
+        # Relevant indices
+        parentIndex = self.determineParentIndex(index)
+        childIndex = index
+
+        # Compare the child node with the parent node and swap if needed
+        while index > 0 and self.nodes[childIndex].priorityLevel > self.nodes[parentIndex].priorityLevel:
+            # Swap the nodes
+            SHelperFunctions.swap_element_in_list(self.nodes , parentIndex , childIndex)
+            
+            # Perform the next iteration to ensure that the highest proiorty has been allocated
+            # Move up to the parent node
+            index = parentIndex  
+            # Calculate the new parent index
+            parentIndex = self.determineParentIndex(index)
+
+    def determineParentIndex(self , index: int) -> int:
+        return (index - 1)//2
+    
+    def dequeue(self) -> Patient: 
+        ''''
+        Returns the top most element in the queue; highest priority patient
+        
+        Dequeue:
+        - First retrieves the root node; the highest priority patient
+        - Removes the last element from the queue
+        - Assigns the last eleement as the root element
+        - Performs deheapifying to ensure that the binary heap properties are retained.
+
+        **Returns:**
+        - `Patient` on top of the queue. Highest Priority patient
+        '''
+        # Ensure that the queue has elements in it
+        if SHelperFunctions.is_empty(self.nodes): # Check if the array is empty
+            raise IndexError("dequeue from an empty priority queue")
+        
+        # Remove the root node to return
+        rootNode = self.nodes[0]
+        # Get the last element of the binary tree.
+        lastNode = self.nodes.pop()
+        
+        if not SHelperFunctions.is_empty(self.nodes): # Ensure that there are nodes in the tree before deheapifying
+            # Replace the root node with the last node
+            self.nodes[0] = lastNode
+
+            # Deheapify the node to ensure that the binary heap properties are upheld.
+            self._deheapify(0)
+        
+        # Return the root node
+        return rootNode
+
+    def _deheapify(self , index : int) -> None:
+        ''' 
+        Normalizes the binary tree by making sure the tree's node are correctly placed 
+        
+        **Parameters:**
+        - `index`: The node's index provided, the starting point of the deheapify.
+        '''
+        # get the length of the nodes
+        nodeLength = len(self.nodes)
+        while  index < nodeLength:
+            # Determine indices. Note: + 1 was added to normalize the array
+            leftChildIndex = index * 2 + 1  # 2n
+            rightChildIndex = (index * 2) + 2 # 2n + 1
+            largestIndex = index # Start with the supplied index
+
+            # Left Child: 2n
+            if (leftChildIndex < nodeLength and self.nodes[leftChildIndex].priorityLevel > self.nodes[largestIndex].priorityLevel):
+                # Move the main index to the child moved
+                largestIndex = leftChildIndex
+                
+            # Right Child: 2n + 1
+            if (rightChildIndex < nodeLength and  self.nodes[rightChildIndex].priorityLevel > self.nodes[largestIndex].priorityLevel):
+                # Move the main index to the child moved
+                largestIndex = rightChildIndex
+
+            # Swap elements if there was a change in the largest index, which indicates that a child had a higher priority
+            if  largestIndex != index:
+                # Swap elements when there was a change in the largest index
+                SHelperFunctions.swap_element_in_list(self.nodes, index, largestIndex)
+                index = largestIndex
+            else: 
+                # Break as the index has not changed, meaning that the current node is the highest node
+                break
+
 
 
