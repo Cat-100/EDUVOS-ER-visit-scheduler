02a49317ab7df7d4985fbacceeba3302fa189363
Cat-100 <saskianlouw.werk7@gmail.com>
2024-10-15 14:36:40 +0200

    Added enum documentation.

--- utils/enums/enums.py
+++ utils/enums/enums.py
@@ -5,10 +5,9 @@ class FileOperation(Enum):
     ''' 
     Denotes file operations when opening files
 
-    - Write: Opens a file for writing, will create the file if it does not exist.
-    - Read: Opens a file for ReadOnluy, allow for reading of a file but not editing the file.
+    - `Write:` Opens a file for writing, will create the file if it does not exist.
+    - `Read:` Opens a file for Read-only, allow for reading of a file but not editing the file.
     '''
-
     # Values
     WRITE = "w"
     READ = "r"
@@ -17,9 +16,9 @@ class FileOperation(Enum):
 class FileOperationalStatus(Enum):
     ''''
     File Status that indicates whether a file operation was:
-    - Successful 
-    - Failed
-    - Stopped
+    - `Successful:` The file operation was successfull. It ran to completion and accomplished it goal.  
+    - `Failed:` The file operation failed, the operation did not run to completion as an exception was thrown.
+    - `Stopped:` The file operation was stopped, this could be due to that the file was already overwritten or that a directory does not need to be recreated.
     '''
     # Values
     SUCCESS = auto()
